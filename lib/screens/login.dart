1a1bef4325018fd3c9c4733f4dbf2aee570b5096
maknoon1a <ahmad.omar@ui.ac.id>
2025-11-19 23:59:10 +0700

    trigger build

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:football_news/screens/register.dart'; // ditambahkan
-
+// 
 void main() {
   runApp(const LoginApp());
 }
